39d57e9b4930ea62372cdb2df03e343ee8cb4c02
Rahul <rahulgupta2019007@gmail.com>
2026-03-28 23:48:10 +0530

    Lecture 11-Classes & Objects

---
+++ 11th.js
@@ -0,0 +1,30 @@
+const student = {
+    fullName: "rahul g",
+    marks: 99,
+    printMarks: function () {
+        console.log("marks = ", this.marks);
+    },
+};
+//JS objects hve a special property called prototype
+
+const employ = {
+    calcTax1() {
+        console.log("taxs are 10%");
+    },
+    // calcTax2 : function () {
+    //     console.log("taxs are 10%");
+    // }
+    //both are correct but we use 1st as it is simple
+};
+
+const karan = {
+    salary: 50000,
+    caltax1() {
+        console.log("taxs are 20%");
+    }
+    //if object & prototype have same method, objects method will be used
+};
+karan.__proto__ = employ;
+
+//class is a program code template for creating objects
+
